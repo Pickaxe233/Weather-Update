21cfacddbc2dfc4fc452726e87a8ac19a4d0b3e0
Pickaxe233 <72426723+Pickaxe233@users.noreply.github.com>
2021-04-06 21:10:35 +0800

    Frame
    框架完成
    页面导航（未完成）
    To Do:
    实际内容

---
+++ MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Weather_Upate
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+        }
+
+        private void All_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                Home.Navigate(typeof(Settings));
+            }
+            else
+            {
+                switch (args.InvokedItem)
+                {
+                    case "HomeButt":
+                        Home.Navigate(typeof(MainPage));
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Settings.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace Weather_Upate
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Settings : Page
+    {
+        public Settings()
+        {
+            this.InitializeComponent();
+        }
+        private async void Github_Click(object sender, RoutedEventArgs e)
+        {
+            var Github = new Uri(@"https://github.com/Pickaxe233/Weather-Update");
+            await Windows.System.Launcher.LaunchUriAsync(Github);
+        }
+
+        private async void Report(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var MyGithub = new Uri(@"https://github.com/Pickaxe233");
+            await Windows.System.Launcher.LaunchUriAsync(MyGithub);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
